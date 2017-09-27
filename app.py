86c4525d3fa5ca549c8800fb7507ea19c33e2d2f
wheeltune <wheeltune@gmail.com>
2017-09-28 02:56:07 +0300

    Init first handlers

--- app.py
+++ app.py
@@ -4,25 +4,28 @@ from threading import Thread
 from telegram import Bot
 from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Updater, Filters
 
+TOKEN = '372828093:AAFIgqIHvneYjO-4PIrN8RF3mP9fzkB88-Q'
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-TOKEN = '372828093:AAFIgqIHvneYjO-4PIrN8RF3mP9fzkB88-Q'
 
 
-def start(bot, update):
+def newevent_handler(bot, update):
     update.message.reply_text('welcome MESSAGE')
 
+def timetable_handler(bot, update):
+    update.message.reply_text('Your time table:\n 10:00-11:20 - Уроки')
 
-def help(bot, update):
-    update.message.reply_text('help message')
-
-
-def echo(bot, update):
-    update.message.reply_text(update.message.text)
+def help_handler(bot, update):
+    update.message.reply_text('Help message should me here :(')
 
 
-def error(bot, update, error):
+def textmessage_handler(bot, update):
+    update.message.reply_text("Sorry, I'm just a reminder bot")
+
+
+def error_handler(bot, update, error):
     logger.warning('Update "%s" caused error "%s"' % (update, error))
 
 # Write your handlers here
@@ -39,14 +42,18 @@ def setup(webhook_url=None):
         updater = Updater(TOKEN)
         bot = updater.bot
         dp = updater.dispatcher
-        dp.add_handler(CommandHandler("start", start))
-        dp.add_handler(CommandHandler("help", help))
+
+        dp.add_handler(CommandHandler("newevent", newevent_handler))
+        dp.add_handler(CommandHandler("timetable", timetable_handler))
+        
+        # dp.add_handler(CommandHandler("start", start))
+        # dp.add_handler(CommandHandler("help", help))
 
         # on noncommand i.e message - echo the message on Telegram
-        dp.add_handler(MessageHandler(Filters.text, echo))
+        dp.add_handler(MessageHandler(Filters.text, textmessage_handler))
 
         # log all errors
-        dp.add_error_handler(error)
+        dp.add_error_handler(error_handler)
     # Add your handlers here
     if webhook_url:
         bot.set_webhook(webhook_url=webhook_url)
