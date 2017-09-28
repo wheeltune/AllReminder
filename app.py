077f054ddb370af187b923a7cf943df659c8284b
wheeltune <wheeltune@gmail.com>
2017-09-28 03:24:57 +0300

    New Event creation

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import logging
 from queue import Queue
 from threading import Thread
+
 from telegram import Bot
 from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Updater, Filters
 
@@ -10,19 +11,49 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+class Event:
+    def __init__(self):
+        self.name = ""
+        self.start_time = 0
+        self.end_time   = 0
+
+class Mode(Enum):
+    NONE        = 0
+    EVENT_NAME  = 1
+    EVENT_START = 2
+    EVENT_END   = 3
+    
+event = Event()
+mode = Mode.NONE
 
 def newevent_handler(bot, update):
-    update.message.reply_text('welcome MESSAGE')
+    global mode
+    if mode == Mode.NONE:
+        mode = Mode.EVENT_NAME
 
 def timetable_handler(bot, update):
-    update.message.reply_text('Your time table:\n 10:00-11:20 - Уроки')
+    global event
+    update.message.reply_text(event.name)
 
 def help_handler(bot, update):
     update.message.reply_text('Help message should me here :(')
 
 
 def textmessage_handler(bot, update):
-    update.message.reply_text("Sorry, I'm just a reminder bot")
+    global mode
+    global event
+    if mode == mode.EVENT_NAME:
+        update.message.reply_text("Enter name of event")
+        event.name = update.message.text
+        mode = mode.EVENT_START
+    elif mode == Mode.EVENT_START:
+        update.message.reply_text("Enter start time dd:mm:yyyy hh:mm:ss")
+        mode = Mode.EVENT_END
+    elif mode == Mode.EVENT_END:
+        update.message.reply_text("Enter end time dd:mm:yyyy hh:mm:ss")
+        mode = Mode.NONE
+    else:
+        update.message.reply_text("Sorry, I'm just a reminder bot")
 
 
 def error_handler(bot, update, error):
